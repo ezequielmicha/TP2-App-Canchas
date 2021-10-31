ac2682a08f202378cfc96d4ada73852752a20171
Fernando Pallas <ferpallas@gmail.com>
2021-10-31 17:22:49 -0300

    coneccion al route courts corregida

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const usersRouter = require('./routes/courts');
+const courtsRouter = require('./routes/courts');
 
 
 
