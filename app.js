eafdae57ce0a0d62c8e5cfc4ffb53856ebc3e86f
Ezequiel <ezequielmicha@gmail.com>
2021-10-31 17:38:00 -0300

    creacion archivos de router y data de pay

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const courtsRouter = require('./routes/courts');
 
-
+const paysRouter = require('./routes/pays');
 
 
 const app = express();
@@ -27,7 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courts', courtsRouter);
 
-
+app.use('/pays', paysRouter);
 
 
 // catch 404 and forward to error handler
